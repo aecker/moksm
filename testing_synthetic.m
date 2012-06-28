0c1523b434cbaf304f28c3d0c5324c8a90182ee1
James Cotton <peabody124@gmail.com>
2012-06-28 15:57:41 +0200

    Make the FullEM method use parfor, which is where most of hte time is spent, and also improve the visualization for synthetic data

--- testing_synthetic.m
+++ testing_synthetic.m
@@ -31,7 +31,30 @@ spike_ids = spike_ids(ids,:);
 
 m = MoKsm(features,spike_times);
 m.params.dTmu = 100;
-m.params.driftRate = 2e-4;
 m = fitModel(m);
 plot(m)
+
+figure
+subplot(121)
+cla
+hold on
+subplot(122)
+cla
+hold on
+c = hsv(units);
+for i = 1:units
+    subplot(121)
+    idx = find(spike_ids == i);
+    plot(spike_times(idx(1:10:end)), features(idx(1:10:end), 1), '.', 'MarkerSize',5, 'color', c(i,:));
+    subplot(122)
+    plot(features(idx(1:10:end), 1), features(idx(1:10:end), 2), '.', 'MarkerSize',5, 'color', c(i,:));
+end
+% Do afterwards so on top of all spikes
+c = jet(units);
+for i = 1:units
+    subplot(121)
+    plot(m.model.mu_t, squeeze(m.model.mu(1,:,i)), 'k','LineWidth',3,'color',c(i,:))
+    subplot(122)
+    plot(squeeze(m.model.mu(1,:,i)),squeeze(m.model.mu(2,:,i)), 'k','LineWidth',3,'color',c(i,:))
+end
 
