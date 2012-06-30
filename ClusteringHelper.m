3fd0ab5070bb15231816002585162ff732986064
James Cotton <peabody124@gmail.com>
2012-06-30 17:04:40 +0200

    Add computing the cross correlations to the ClusteringHelper

--- ClusteringHelper.m
+++ ClusteringHelper.m
@@ -38,7 +38,7 @@ classdef ClusteringHelper
         function ids = getClusterIds(self)
             % Returns the valid cluster ids
             ids = 1:length(self.ClusterAssignment);
-            containGroups = find(cellfun(@(x) any(x < 0), ids));
+            containGroups = find(cellfun(@(x) any(x < 0), self.ClusterAssignment));
             
             toRemove = [];
             for i = 1:length(containGroups)
@@ -52,7 +52,7 @@ classdef ClusteringHelper
             % Remove any cluster ids contained by other cluster ids
             ids(unique(toRemove)) = [];
         end
-        
+                        
         function spikeIds = getSpikesByClusIds(self,clusterIds)
             spikeIds = cat(1,self.ClusterAssignment{clusterIds});
             
@@ -73,8 +73,26 @@ classdef ClusteringHelper
         % TODO: Probably there should be a private method for actually
         % adding and deleting clusters from updateInformation so the
         % tags follow the clusters around properly
-    end
     
+        function [corrs time] = getCrossCorrs(self, varargin)
+            params.clusIds = getClusterIds(self);
+            params.binSize = 1;
+            params.nBins = 30;
+            params = parseVarArgs(params,varargin{:});
+            
+            for i = 1:length(params.clusIds)
+                ids1 = getSpikesByClusIds(self,params.clusIds(i));
+                t1 = getSpikeTimes(self, ids1);
+                
+                for j = i:length(params.clusIds)
+                    ids2 = getSpikesByClusIds(self,params.clusIds(j));
+                    t2 = getSpikeTimes(self,ids2);
+                    [corrs{i,j} time] = CrossCorr(t1,t2,params.binSize,params.nBins);
+                end
+            end
+        end
+    end
+
     methods (Abstract)
         % This method updates the ClusterAssignment and ContaminationMatrix
         self = updateInformation(self);
